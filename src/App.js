efae186cd0f299582d55fb27a7f63602e593f2d7
Joaquin Barros <joaquinjbt@gmail.com>
2024-04-21 20:18:18 -0400

    local storage

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import { TodoList } from './components/TodoList'
 import { TodoItem } from './components/TodoItem';
 import { CreateTodoBtn } from './components/CreateTodoBtn';
 
-const defaultTodos = [
-  {text: 'Terminar curso de React.js', completed: false},
-  {text: 'Ver Kill Bill Vol. 1', completed: false},
-  {text: 'Taskete Kudasai', completed: false},
-  {text: 'Cortar cebolla', completed: true},
-  {text: 'Comer chimichanga', completed: false},
-]
+// const defaultTodos = [
+//   {text: 'Terminar curso de React.js', completed: false},
+//   {text: 'Ver Kill Bill Vol. 1', completed: false},
+//   {text: 'Taskete Kudasai', completed: false},
+//   {text: 'Cortar cebolla', completed: true},
+//   {text: 'Comer chimichanga', completed: false},
+// ]
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1', defaultTodos)
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  let localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -27,6 +41,12 @@ function App() {
       return todoText.includes(searchText);
     }
   );
+
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+    
+    setTodos(newTodos);
+  };
 
   const finishTodo = (text) => {
     const newTodos = [...todos];
@@ -41,7 +61,7 @@ function App() {
       todoStatus.completed = true;
     }
     
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -50,7 +70,7 @@ function App() {
       (todo) => todo.text === text
     );
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
